9b35e7857d777c60d94481b7fdd1cdadbb235051
Matthew Yuen <yuenm18@up.edu>
2020-01-11 17:33:51 -0800

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+  },
+  'rules': {
+    'object-curly-spacing': [2, 'always'],
+    'linebreak-style': 0,
+  },
+};
